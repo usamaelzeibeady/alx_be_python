4818f7e4ff2fc14dc4ca51864feefdbd9f3da2a8
usamaelzeibeady <usama.elzeibeady@gmail.com>
2024-12-16 17:45:49 +0200

    رفع المجلد مع الملفات

--- Desktop/python_introduction/future_age_calculator.py
+++
@@ -1,3 +0,0 @@
-current_age = int(input("How old are you? "))
-future_age = current_age + 27
-print(f"In 2050, you will be {future_age} years old.")
--- Desktop/python_introduction/hours_to_seconds.py
+++
@@ -1,3 +0,0 @@
-hours = 2
-seconds = hours * 60 * 60
-print(f"{hours} hour(s) is {seconds} seconds.")
